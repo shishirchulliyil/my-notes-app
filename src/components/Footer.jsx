621cac6039ce00892d02bc9486a49b3eae73ef0c
shishir c <shishir@shishirs-mbp.gateway.sasktel.net>
2020-05-10 17:37:35 -0600

    front end of my notes app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  //const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Made with ❤️ </p>
+      <p>Shishir</p>
+    </footer>
+  );
+}
+
+export default Footer;
