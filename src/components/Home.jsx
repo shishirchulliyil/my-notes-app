467d5b774fb50f198ac784dd06994e27abd69fc7
shishir c <shishir@shishirs-mbp.gateway.sasktel.net>
2020-05-17 13:48:36 -0600

    using react-router-dom to create 404 page for non matching paths

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,14 @@ import App from "./App";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function Home() {
+  function NoMatch({ location }) {
+    return (
+      <div>
+        <h3>404 {location.pathname} Not Found</h3>
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <div>
@@ -11,6 +19,7 @@ function Home() {
           <Route path="/" exact component={Login} />
           <Route path="/login" exact component={Login} />
           <Route path="/notes" exact component={App} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     </BrowserRouter>
